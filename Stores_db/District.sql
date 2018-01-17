b27749f88d45bf85fdcabfa95cad9709e3f8794f
livv52 <livia.anghel.11@gmail.com>
2018-01-17 23:15:02 +0100

    tables created

---
+++ Stores_db/District.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[District]
+(
+	[DistrictId] INT IDENTITY (1, 1) NOT NULL,
+	[Name] VARCHAR (50) NOT NULL,
+	[StoreId] INT NOT NULL,
+	CONSTRAINT [PK_dbo.District] PRIMARY KEY CLUSTERED ([DistrictId] ASC),
+	CONSTRAINT [FK_dbo.District_dbo.Store_StoreId] FOREIGN KEY ([StoreId]) REFERENCES [dbo].[Store] ([StoreId])
+)
+
+GO
+CREATE NONCLUSTERED INDEX [IX_StoreId]
+    ON [dbo].[District]([StoreId] ASC);
---
+++ Stores_db/DistrictSalesperson.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE [dbo].[DistrictSalesperson]
+(
+	[DistrictSalespersonId] INT IDENTITY (1, 1) NOT NULL,
+	[SPId] INT NOT NULL,
+	[DistrictId] INT NOT NULL,
+	CONSTRAINT [PK_dbo.DistrictSalesperson] PRIMARY KEY CLUSTERED ([DistrictSalespersonId] ASC),
+    CONSTRAINT [FK_dbo.DistrictSalesperson_dbo.Salesperson_SPId] FOREIGN KEY ([SPId]) REFERENCES [dbo].[Salesperson] ([SPId]),
+	CONSTRAINT [FK_dbo.DistrictSalesperson_dbo.District_DistrictId] FOREIGN KEY ([DistrictId]) REFERENCES [dbo].[District] ([DistrictId])
+
+)
+GO
+CREATE NONCLUSTERED INDEX [IX_SPId]
+    ON [dbo].[DistrictSalesperson]([SPId] ASC);
+
+GO
+CREATE NONCLUSTERED INDEX [IX_DistrictId]
+    ON [dbo].[DistrictSalesperson]([DistrictId] ASC);
